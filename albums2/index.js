922ac75901f6564c7bcde5ed4e1c7315047669f1
thembones79 <michal.szumnarski@gmail.com>
2019-01-15 23:40:39 +0100

    header shadows

--- albums2/index.js
+++ albums2/index.js
@@ -1,7 +1,14 @@
-/** @format */
-
+// Import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import Header from './src/components/header';
 
-AppRegistry.registerComponent(appName, () => App);
+// Create a component
+const App = () => {
+    return (
+        <Header headerText={'Albums'}/>
+    );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums2', ()=>App);
