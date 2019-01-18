4b4f2eac21d75a7b87dd18e885fcce64dac5a2a2
thembones79 <michal.szumnarski@gmail.com>
2019-01-18 22:54:43 +0100

    v

--- auth5/src/Auth.js
+++ auth5/src/Auth.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import {Header} from './components/common';
+import firebase from 'firebase';
 
 class Auth extends Component {
   render() {
     return (
       <View>
           <Header headerText="Authentication" />
-        <Text>dupa</Text>
+        <Text>dupa2</Text>
       </View>
     );
   }
