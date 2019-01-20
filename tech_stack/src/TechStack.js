e9525cf87a0693b1b90a323b54b25f8995b10d2b
thembones79 <michal.szumnarski@gmail.com>
2019-01-20 16:18:13 +0100

    reducer boilerplate

--- tech_stack/src/TechStack.js
+++ tech_stack/src/TechStack.js
@@ -2,14 +2,18 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducers from './reducers';
+import reducers from "./reducers";
+import { Header } from "./components/common";
 
 const TechStack = () => {
   return (
-      <Provider store={createStore(reducers)}>
-    <View>
-      <Text>Dupa</Text>
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View>
+          <Header headerText='Tech Stack'/>
+        <View>
+          <Text>Dupa</Text>
+        </View>
+      </View>
     </Provider>
   );
 };
