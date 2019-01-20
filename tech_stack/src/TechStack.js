c8fce782bc192870e0d9b9b76e22f4560599153b
thembones79 <michal.szumnarski@gmail.com>
2019-01-20 15:09:39 +0100

    added tecfstack boilerplate

---
+++ tech_stack/src/TechStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class TechStack extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>
+                    Dupa
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default TechStack;
