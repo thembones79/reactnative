734253027622821763338997064a61429e0983ea
thembones79 <michal.szumnarski@gmail.com>
2019-01-20 21:46:28 +0100

    added linrary items and flatlist

--- tech_stack/src/TechStack.js
+++ tech_stack/src/TechStack.js
@@ -4,15 +4,14 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
 import { Header } from "./components/common";
+import LibraryList from "./components/LibraryList";
 
 const TechStack = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
-          <Header headerText='Tech Stack'/>
-        <View>
-          <Text>Dupa</Text>
-        </View>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
