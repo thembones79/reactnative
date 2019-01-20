ddf2a5a2ea009b6e9202497ec47119105b0da1b5
thembones79 <michal.szumnarski@gmail.com>
2019-01-20 23:29:30 +0100

    rendered list of titles

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -4,15 +4,17 @@ import { connect } from "react-redux";
 import ListItem from "./ListItem";
 
 class LibraryList extends Component {
+  /*
   renderItem(library) {
     return <ListItem library={library} />;
   }
+*/
 
   render() {
     return (
       <FlatList
         data={this.props.libraries}
-        renderItem={this.renderItem}
+        renderItem={({ item }) => <ListItem library={item} />}
         keyExtractor={library => library.id}
       />
     );
--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import { Text } from "react-native";
+import { connect } from "react-redux";
 import { CardSection } from "./common";
+import * as actions from "../actions";
 
 class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
+
     return (
       <CardSection>
-        <Text>{this.props.library.title}</Text>
+        <Text style={titleStyle}>{this.props.library.title}</Text>
       </CardSection>
     );
   }
 }
 
-export default ListItem;
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+export default connect(
+  null,
+  actions
+)(ListItem);
