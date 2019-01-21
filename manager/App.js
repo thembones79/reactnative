504813593651c7f75d706115c902c35c2cff563f
thembones79 <michal.szumnarski@gmail.com>
2019-01-21 23:00:43 +0100

    first commit

---
+++ manager/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Manager from './src/Manager';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Manager/>
+    );
+  }
+}
---
+++ manager/src/Manager.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import firebase from "firebase";
+import { Text, View } from "react-native";
+import reducers from "./reducers";
+
+class Manager extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAhfF8kD419QdXUUXzxnFqDou4AH6l8-_I",
+      authDomain: "manager-470e5.firebaseapp.com",
+      databaseURL: "https://manager-470e5.firebaseio.com",
+      projectId: "manager-470e5",
+      storageBucket: "manager-470e5.appspot.com",
+      messagingSenderId: "168766344006"
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Dupa2</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default Manager;
