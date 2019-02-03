cccb368f6202fcc388031f0a8c3d625aa07213dc
thembones79 <michal.szumnarski@gmail.com>
2019-02-03 23:55:31 +0100

    added PanResponder for gestures

---
+++ swipe/src/Deck.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { View, Animated, PanResponder } from "react-native";
+
+class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {},
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder };
+  }
+
+  renderCards() {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  }
+
+  render() {
+    //this.state.panResponder;
+    return (
+       <View {...this.state.panResponder.panHandlers}>
+           {this.renderCards()}
+           </View>
+    );
+    
+    
+  }
+}
+
+export default Deck;
