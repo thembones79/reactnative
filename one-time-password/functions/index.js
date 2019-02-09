0e8cdfb75437c53e073ca2ef0fbcc911f90b5833
thembones79 <michal.szumnarski@gmail.com>
2019-02-09 19:13:03 +0100

    added twilio

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,8 +1,15 @@
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const createUser = require("./create_user");
+const serviceAccount = require("./service_account.json");
+const requestOneTimePassword = require("./request_one_time_password.js");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-response.send("Hello from Firebase!");
- });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-8.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
+exports.requestOneTimePassword = functions.https.onRequest(
+  requestOneTimePassword
+);
