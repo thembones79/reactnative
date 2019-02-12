2cbf08252300ab643ab5fc9ba11607def23be474
thembones79 <michal.szumnarski@gmail.com>
2019-02-12 23:49:03 +0100

    it works

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+
+componentDidMount(){
+  const config = {
+    apiKey: "AIzaSyASGp0hJDmL_-ldeNEVf0UlRmsnMchjiL0",
+    authDomain: "one-time-password-8.firebaseapp.com",
+    databaseURL: "https://one-time-password-8.firebaseio.com",
+    projectId: "one-time-password-8",
+    storageBucket: "one-time-password-8.appspot.com",
+    messagingSenderId: "387240407096"
+  };
+  firebase.initializeApp(config);
+}
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,6 +33,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
--- firebase-auth/components/SignInForm.js
+++ firebase-auth/components/SignInForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Input, Button} from 'react-native-elements';
 import axios from 'axios';
+import firebase from 'firebase';
 
 
 const ROOT_URL = "https://us-central1-one-time-password-8.cloudfunctions.net";
@@ -16,23 +17,25 @@ class SignInForm extends Component {
 
 
         try {
-            await axios.post(`${ROOT_URL}/verifyOneTimePassword`, {phone, code})
+            let {data} = await axios.post(`${ROOT_URL}/verifyOneTimePassword`, {phone, code})
+       // console.log(response);
+       firebase.auth().signInWithCustomToken(data.token);
         }catch (err) {
-            console.log(err);
+      //      console.log(err);
         }
     }
 
     render() {
         return(
             <View>
-                <View>
+                <View style={{marginBottom: 10}}>
                     <Text>Enter Phone Number</Text>
                     <Input 
                     value={this.state.phone}
                     onChangeText={phone=>this.setState({phone})}
                     />
                 </View>
-                <View>
+                <View style={{marginBottom: 10}}>
                     <Text>Enter Code</Text>
                     <Input 
                     value={this.state.code}
