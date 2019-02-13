bcd673a2ed3b554fd9cb903bef60939ed3fe0678
thembones79 <michal.szumnarski@gmail.com>
2019-02-14 00:30:22 +0100

    added React Navigation

--- jobs/App.js
+++ jobs/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+
+const MainNavigator = createBottomTabNavigator({
+  welcome: {screen: WelcomeScreen},
+  auth: {screen: AuthScreen}
 });
+
+
+
+
+
+
+export default createAppContainer(MainNavigator);
+
+
+
