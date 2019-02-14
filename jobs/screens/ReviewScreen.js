eff85d02039e15678e51e3c2ee00e205890484c3
thembones79 <michal.szumnarski@gmail.com>
2019-02-14 22:32:36 +0100

    some errors to fix

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Button} from 'react-native-elements';
+import { navigation  } from "react-navigation";
 
 class ReviewScreen extends Component {
     static navigationOptions = {
-        
-    }
+        title: "Review Jobs",
+        headerRight:  <Button title="Settings" onPress={()=>navigation.navigate('settings') } />
+        }
+    
     render(){
         return(
             <View>
