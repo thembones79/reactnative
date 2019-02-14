aa9e755e69097237dd11dd9b2459b6f9eef8f8fd
thembones79 <michal.szumnarski@gmail.com>
2019-02-14 07:26:17 +0100

    nesting navigation

--- jobs/App.js
+++ jobs/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
-import AuthScreen from './screens/AuthScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-
-
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  createStackNavigator
+} from "react-navigation";
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 const MainNavigator = createBottomTabNavigator({
-  welcome: {screen: WelcomeScreen},
-  auth: {screen: AuthScreen}
+  welcome: { screen: WelcomeScreen },
+  auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: {
+        screen: createStackNavigator({
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen }
+        })
+      }
+    })
+  }
 });
 
-
-
-
-
-
 export default createAppContainer(MainNavigator);
-
-
-
