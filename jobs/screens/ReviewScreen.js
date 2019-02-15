3f952ff5c809794b86082381374e7019807f77a8
thembones79 <michal.szumnarski@gmail.com>
2019-02-15 07:43:23 +0100

    added some styling to the button in navbar

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,38 +1,42 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Button} from 'react-native-elements';
-
+import React, { Component } from "react";
+import { View, Text, Platform } from "react-native";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
-    
-    static navigationOptions = ({ navigation }) => {
-        return({
-        title: "Review Jobs",
-        headerRight:  (
-        <Button title="Settings" onPress={()=>navigation.navigate('settings') } />
-        )
-        }
-        )
-        }
-    
-    render(){
-        
-        return(
-            <View>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-            </View>
-        );
-    }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Review Jobs",
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigation.navigate("settings")}
+          icon={{ name: "code", color: "white" }}
+          buttonStyle={{backgroundColor: "red"}}
+        />
+      ),
+      style: {
+          marginTop: Platform.OS==='android' ? 24 : 0
+      }
+    };
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
 }
 
 export default ReviewScreen;
