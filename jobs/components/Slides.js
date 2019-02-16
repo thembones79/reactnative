300ae51cdd12d15739f1cea5846fd944f8346e62
thembones79 <michal.szumnarski@gmail.com>
2019-02-16 16:40:39 +0100

    testing

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,38 +1,34 @@
-import React, {Component} from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
 
 class Slides extends Component {
-    renderSlides(){
-        return this.props.data.map((slide)=>{
-            return(
-            <View key={slide.text} style={styles.slideStyle}>
-                <Text style={styles.slideText}>{slide.text}</Text>
-            </View>
-            );
-        });
-    }
-    render(){
-        return(
-          <ScrollView
-          horizontal
-          style={{flex:1}}
-          >
-{this.renderSlides()}
-
-          </ScrollView>
-          );
-    }
+  renderSlides() {
+    return this.props.data.map(slide => {
+      return (
+        <View key={slide.text} style={styles.slideStyle}>
+          <Text style={styles.slideText}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+  render() {
+    return (
+      <ScrollView horizontal style={{ flex: 1 }}>
+        {this.renderSlides()}
+      </ScrollView>
+    );
+  }
 }
 
 const styles = {
-    slideStyle: {
-        flex: 1,
-        justifyContent: "center",
-        aligItems: "center"
-    },
-    slideText:{
-        fontSize: 30
-    }
-}
+  slideStyle: {
+    flex: 1,
+    justifyContent: "center",
+    aligItems: "center"
+  },
+  slideText: {
+    fontSize: 30
+  }
+};
 
 export default Slides;
