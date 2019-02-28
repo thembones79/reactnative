5e54f3faca58d75a46f211fdd32b298c261d48f3
thembones79 <michal.szumnarski@gmail.com>
2019-02-28 21:15:56 +0100

    updating redux-persist

--- jobs/reducers/index.js
+++ jobs/reducers/index.js
@@ -1,10 +1,23 @@
-import { combineReducers } from "redux";
-import auth from "./auth_reducer";
-import jobs from "./job_reducer";
-import likedLobs from './likes_reducer';
+//import { combineReducers  } from "redux";
+import { persistCombineReducers } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+import auth from './auth_reducer';
+import jobs from './jobs_reducer';
+import likedJobs from './likes_reducer';
 
-export default combineReducers({
-  auth: auth,
-  jobs: jobs,
-  likedJobs: likedLobs
+// redux persist v5 fix
+const config = {
+
+  key: 'jobkey',
+
+  storage: AsyncStorage,
+
+  whitelist: ['likedJobs']
+
+};
+
+export default persistCombineReducers(config, {
+  auth, // same as auth: auth es6
+  jobs,
+  likedJobs
 });
--- jobs/store/index.js
+++ jobs/store/index.js
@@ -1,18 +1,19 @@
-import {createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { autoRehydrate, persistStore } from  'redux-persist';
-import { AsyncStorage } from 'react-native';
-import reducers from '../reducers';
+import reducers from '../reducers'; // do not have to specify index.js
+import { persistStore } from 'redux-persist';
+//import { AsyncStorage } from 'react-native';
+import { Location } from "expo";
 
 const store = createStore(
-    reducers,
-    {},
-    compose(
-        applyMiddleware(thunk),
-        autoRehydrate()
+    reducers, // number of reducers
+    {}, // default state empty in the beginning
+    compose( // applies all the middle ware used in the app
+        applyMiddleware(thunk)
+        //,autoRehydrate()
     )
 );
 
-persistStore(store, {storage: AsyncStorage, whitelist: ['likedJobs']});
+persistStore(store, null, null);// used to have this as well , whitelist: ['likedJobs']  but change in v5
 
 export default store;
