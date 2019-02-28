da1e0b222963d688792f2d58c8501b11446b408b
thembones79 <michal.szumnarski@gmail.com>
2019-02-28 22:08:38 +0100

    rehydrate compatibility improvement

--- jobs/reducers/index.js
+++ jobs/reducers/index.js
@@ -2,7 +2,7 @@
 import { persistCombineReducers } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
 import auth from './auth_reducer';
-import jobs from './jobs_reducer';
+import jobs from './job_reducer';
 import likedJobs from './likes_reducer';
 
 // redux persist v5 fix
